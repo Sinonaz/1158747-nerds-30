95d3a2a726832a57fa4308a27ea52272f31bf33e
Az <53692366+Sinonaz@users.noreply.github.com>
2021-02-23 19:11:02 +0300

    prepare to the finish

--- js/script.js
+++ js/script.js
@@ -1,42 +1,97 @@
 var button = document.querySelector('.user_button');
 var popup = document.querySelector('.modal_container');
-var shadow = document.querySelector('.modal_shadow');
 var close = document.querySelector('.modal_close');
 var form = popup.querySelector('.modal_form');
 var login = popup.querySelector('.input_login');
 var email = popup.querySelector('.input_email');
-var storage = localStorage.getItem('input_login');
+
+let isStorageSupport = true;
+let storage = '';
+
+try {
+  storage = localStorage.getItem('login');
+} catch (err) {
+  isStorageSupport = false;
+}
 
 // OPEN_MODAL
 
-button.addEventListener('click', function (event) {
-  event.preventDefault();
+button.addEventListener('click', function (evt) {
+  evt.preventDefault();
   popup.classList.add('modal_open');
-  shadow.classList.add('modal_shadow_block');
   popup.classList.remove('bounceOutDown');
-    if (storage) {
-      login.value = storage;
-      mail.focus();
+  if (storage) {
+    login.value = storage;
+    mail.focus();
   } else {
-      login.focus();
+    login.focus();
   }
 });
 
 // CLOSE_MODAL
 
-close.addEventListener('click', function (event) {
-  event.preventDefault();
+close.addEventListener('click', function (evt) {
+  evt.preventDefault();
   popup.classList.add('bounceOutDown');
-  window.setTimeout(function() {
+  popup.classList.remove('shake');
+  window.setTimeout(function () {
     popup.classList.remove('modal_open');
-    shadow.classList.remove('modal_shadow_block');
   }, 500);
 });
 
-shadow.addEventListener("click", function(event) {
-  event.preventDefault();
-  window.setTimeout(function() {
-    popup.classList.remove('modal_open');
-    shadow.classList.remove('modal_shadow_block');
-  }, 500);
+// CLOSE_ESC
+
+window.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === 27) {
+    if (popup.classList.contains('modal_open')) {
+      popup.classList.add('bounceOutDown');
+      window.setTimeout(function () {
+        popup.classList.remove('modal_open');
+      }, 500);
+      popup.classList.remove('shake');
+    }
+  }
+});
+
+// SEND_FOMR
+
+form.addEventListener('submit', function (evt) {
+  if (!login.value || !password.value) {
+    evt.preventDefault();
+    popup.classList.remove('shake');
+    popup.offsetWidth = popup.offsetWidth;
+    popup.classList.add('shake');
+  } else {
+    if (isStorageSupport) {
+      localStorage.setItem('login', loginLogin.value);
+    }
+  }
+});
+
+// SLIDER
+
+var advantages = document.querySelector('.advantages');
+var label1 = advantages.querySelector('.label-1');
+var label2 = advantages.querySelector('.label-2');
+var label3 = advantages.querySelector('.label-3');
+var slide1 = advantages.querySelector('.slide-1');
+var slide2 = advantages.querySelector('.slide-2');
+var slide3 = advantages.querySelector('.slide-3');
+
+label1.addEventListener('click', function (evt) {
+  slide1.classList.remove('slide_hidden');
+  slide2.classList.add('slide_hidden');
+  slide3.classList.add('slide_hidden');
+});
+
+label2.addEventListener('click', function (evt) {
+  slide1.classList.add('slide_hidden');
+  slide2.classList.remove('slide_hidden');
+  slide3.classList.add('slide_hidden');
+});
+
+label3.addEventListener('click', function (evt) {
+  slide1.classList.add('slide_hidden');
+  slide2.classList.add('slide_hidden');
+  slide3.classList.remove('slide_hidden');
 });
